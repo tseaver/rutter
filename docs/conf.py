0d2ecae92df79f2803d534bfd2133b79d62742ec
Steve Piercy <web@stevepiercy.com>
2020-08-10 14:23:41 -0700

    Ignore localhost URLs for linkcheck

--- docs/conf.py
+++ docs/conf.py
@@ -244,3 +244,9 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'http://docs.python.org/': None}
+
+# List of items to ignore when running linkcheck
+linkcheck_ignore = [
+    r'http://localhost:\d+',
+]
+
